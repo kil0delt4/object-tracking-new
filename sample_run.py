7db5267f709313426413f0a2a743d28f1ccafd50
kil0delt4 <51435460+kil0delt4@users.noreply.github.com>
2023-04-11 11:54:04 +0530

    initial commit

---
+++ sample_run.py
@@ -0,0 +1,5 @@
+from detection_function import run
+
+# det=Detection(source="dataset/images/test.jpeg", save_txt=True, save_conf=True)
+
+run(r_source="dataset/images/test.jpeg", r_save_txt=True, r_save_conf=True)
